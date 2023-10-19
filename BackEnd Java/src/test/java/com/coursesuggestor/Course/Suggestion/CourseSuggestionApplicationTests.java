0979988f1e90671b3c047b5f7749b34ea6071fbf
Dhvanik <pateldhvanik789@gmail.com>
2023-10-19 13:00:28 +0530

    Add files via upload

---
+++ BackEnd Java/src/test/java/com/coursesuggestor/Course/Suggestion/CourseSuggestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursesuggestor.Course.Suggestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSuggestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
